e57cd23db4dae2e230b9ddd7fdf694d52c3d298a
Yuvaraaj17 <yuvaraaj5910@gmail.com>
2023-12-16 19:43:47 +0530

    simple todo list

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react"
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+  
+  const [taskName,settaskName]=useState("") // state for storing taskname from input field
+  const [id,setId]=useState(1) // state of id
+  const [tasks,setTasks]=useState([]) // state of list of task
+  
+  
+  const change = (event)=>{ // caputre change in input field 
+    settaskName(event.target.value) 
+  }
 
+  const addItem=()=>{ // add new task
+    var obj={id: id,name: taskName,checked: false}
+    tasks.push(obj)
+    setTasks(tasks)
+    settaskName("")
+    setId(id+1)
+  }
+  
+ const update=(id)=>{ // update state of task
+  const newlist=tasks.map(
+    (task)=>
+      task.id===id ? {...task,checked:!task.checked} : task)
+  setTasks(newlist)
+ }
+
+ const remove=()=>{ // remove finished/checked tasks
+  const newtasks=tasks.filter((task)=>task.checked==false);
+  setTasks(newtasks)
+  console.log(newtasks)
+ }
+
+  return(
+    <div>
+      <p>Simple To do List</p>
+      <input type="text" value={taskName} onChange={(event) =>change(event)}/>
+      <div>
+        <button onClick={addItem}>Add</button>
+        <button onClick={remove}>Remove</button>
+      </div>
+      <ul>
+      {tasks.map((task)=>(
+        <li key={task.id}>
+          {task.name}
+          <input type="checkbox" checked={task.checked} onChange={()=>{update(task.id)}}/>
+        </li>
+      ))}  
+      </ul>
+    </div>
+  )
+}
 export default App;
---
+++ src/Click.js
@@ -0,0 +1,10 @@
+// const Click = ()=>{
+//     var a=1
+//     return(
+//         <div>
+//         <input type="button" value="Click me" onClick={(e)=>{handleClick(e)}}/>
+//     </div>
+//     )
+    
+// }
+// export default Click;
